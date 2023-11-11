e773b1f940fabe572cfb5b894b45ca016631ae97
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-11-11 12:40:38 -0300

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
 echo "Bem-vindo ao Script de Conversão!"
-read -p "Digite o nome do arquivo de entrada (com extensão): " arquivo_entrada
-read -p "Digite o nome do arquivo de saída (com extensão): " arquivo_saida
+read -p "Digite o caminho completo do arquivo de entrada (com extensão): " caminho_entrada
+read -p "Digite o caminho completo do arquivo de saída (com extensão): " caminho_saida
+
+arquivo_entrada=$(basename "$caminho_entrada")
+arquivo_saida=$(basename "$caminho_saida")
 
 extensao_entrada=$(echo "$arquivo_entrada" | awk -F'.' '{print tolower($NF)}')
 extensao_saida=$(echo "$arquivo_saida" | awk -F'.' '{print tolower($NF)}')
 
 case "$extensao_entrada" in
     pdf)
-        convert -density 300 "$arquivo_entrada" -quality 100 "$arquivo_saida"
+        convert -density 300 "$caminho_entrada" -quality 100 "$caminho_saida"
         echo "Conversão de PDF para imagem concluída!"
         ;;
     docx|doc)
-        libreoffice --headless --convert-to pdf --outdir $(dirname "$arquivo_saida") "$arquivo_entrada"
+        libreoffice --headless --convert-to pdf --outdir "$(dirname "$caminho_saida")" "$caminho_entrada"
         echo "Conversão de DOCX/DOC para PDF concluída!"
         ;;
     mp3)
-        ffmpeg -i "$arquivo_entrada" "$arquivo_saida"
+        ffmpeg -i "$caminho_entrada" "$caminho_saida"
         echo "Conversão de MP3 para WAV concluída!"
         ;;
     mp4|avi)
-        ffmpeg -i "$arquivo_entrada" -vf "fps=10,scale=320:-1:flags=lanczos" -c:v gif "$arquivo_saida"
+        ffmpeg -i "$caminho_entrada" -vf "fps=10,scale=320:-1:flags=lanczos" -c:v gif "$caminho_saida"
         echo "Conversão de MP4/AVI para GIF concluída!"
         ;;
     jpg|jpeg|png|gif)
-        convert "$arquivo_entrada" "$arquivo_saida"
+        convert "$caminho_entrada" "$caminho_saida"
         echo "Conversão de imagem para PDF concluída!"
         ;;
     txt)
-        cp "$arquivo_entrada" "$arquivo_saida"
+        cp "$caminho_entrada" "$caminho_saida"
         echo "Cópia do arquivo de texto concluída!"
         ;;
     folder)
@@ -47,11 +50,11 @@ case "$extensao_entrada" in
             exit 1
         fi
 
-        ffmpeg -framerate 1/"$duracao" -pattern_type glob -i "$pasta_entrada/*.png" -c:v libx264 -r 30 "$arquivo_saida"
+        ffmpeg -framerate 1/"$duracao" -pattern_type glob -i "$pasta_entrada/*.png" -c:v libx264 -r 30 "$caminho_saida"
         echo "Conversão de imagens para vídeo concluída!"
         ;;
     flac)
-        ffmpeg -i "$arquivo_entrada" "$arquivo_saida"
+        ffmpeg -i "$caminho_entrada" "$caminho_saida"
         echo "Conversão de FLAC concluída!"
         ;;
     *)
