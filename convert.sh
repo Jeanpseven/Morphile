d2744b2729fd12e128cac830727a51539f0d8610
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-11-11 13:45:14 -0300

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,71 +1,50 @@
 #!/bin/bash
 
-echo "Bem-vindo ao Script de Conversão!"
-read -p "Digite o caminho do arquivo de entrada (com extensão): " input_path
-read -p "Digite o caminho do arquivo de saída (com extensão ou nome se for salvar no repo local): " output_path
+converter() {
+    arquivo_entrada=$1
+    arquivo_saida=$2
 
-# Remover aspas, se presentes
-input_path="${input_path//\"/}"
-input_path="${input_path//\'/}"
-output_path="${output_path//\"/}"
-output_path="${output_path//\'/}"
+    extensao_entrada="${arquivo_entrada##*.}"
+    extensao_saida="${arquivo_saida##*.}"
 
-# Verificar se a saída é um diretório
-if [ -d "$output_path" ]; then
-    output_path="$output_path/$(basename "$input_path")"
-fi
-
-extension="${input_path##*.}"
-
-case "$extension" in
-    pdf)
-        convert -density 300 "$input_path" -quality 100 "$output_path"
-        echo "Conversão de PDF para imagem concluída!"
-        ;;
-    docx|doc)
-        libreoffice --headless --convert-to pdf --outdir $(dirname "$output_path") "$input_path"
-        echo "Conversão de DOCX/DOC para PDF concluída!"
-        ;;
-    mp3)
-        ffmpeg -i "$input_path" "$output_path"
-        echo "Conversão de MP3 para WAV concluída!"
-        ;;
-    mp4|avi)
-        ffmpeg -i "$input_path" -vf "fps=10,scale=320:-1:flags=lanczos" -c:v gif "$output_path"
-        echo "Conversão de MP4/AVI para GIF concluída!"
-        ;;
-    jpg|jpeg|png|gif)
-        convert "$input_path" "$output_path"
-        echo "Conversão de imagem para PDF concluída!"
-        ;;
-    txt)
-        cp "$input_path" "$output_path"
-        echo "Cópia do arquivo de texto concluída!"
-        ;;
-    flac)
-        ffmpeg -i "$input_path" -c:a flac "$output_path"
-        echo "Conversão de FLAC concluída!"
-        ;;
-    folder)
-        read -p "Digite o caminho da pasta contendo imagens: " folder_path
-        read -p "Digite a duração de cada imagem em segundos: " duration
-        read -p "Digite o caminho completo do arquivo de saída (com extensão): " output_path_folder
-
-        if [ ! -d "$folder_path" ]; then
-            echo "Erro: Pasta de entrada não encontrada."
+    case "$extensao_entrada" in
+        pdf)
+            convert -density 300 "$arquivo_entrada" -quality 100 "$arquivo_saida"
+            echo "Conversão de PDF para imagem concluída!"
+            ;;
+        docx|doc)
+            libreoffice --headless --convert-to pdf --outdir $(dirname "$arquivo_saida") "$arquivo_entrada"
+            echo "Conversão de DOCX/DOC para PDF concluída!"
+            ;;
+        mp3)
+            ffmpeg -i "$arquivo_entrada" "$arquivo_saida"
+            echo "Conversão de MP3 para WAV concluída!"
+            ;;
+        mp4|avi)
+            ffmpeg -i "$arquivo_entrada" -vf "fps=10,scale=320:-1:flags=lanczos" -c:v gif "$arquivo_saida"
+            echo "Conversão de MP4/AVI para GIF concluída!"
+            ;;
+        jpg|jpeg|png|gif)
+            convert "$arquivo_entrada" "$arquivo_saida"
+            echo "Conversão de imagem concluída!"
+            ;;
+        txt)
+            cp "$arquivo_entrada" "$arquivo_saida"
+            echo "Cópia do arquivo de texto concluída!"
+            ;;
+        flac)
+            ffmpeg -i "$arquivo_entrada" "$arquivo_saida"
+            echo "Conversão de FLAC concluída!"
+            ;;
+        *)
+            echo "Tipo de arquivo não suportado. Saindo."
             exit 1
-        fi
+            ;;
+    esac
+}
 
-        if [ -z "$output_path_folder" ]; then
-            echo "Erro: Caminho de saída inválido."
-            exit 1
-        fi
+# Exemplo de uso
+read -p "Digite o nome do arquivo de entrada (com extensão): " arquivo_entrada
+read -p "Digite o nome do arquivo de saída (com extensão): " arquivo_saida
 
-        ffmpeg -framerate 1/"$duration" -pattern_type glob -i "$folder_path/*.png" -c:v libx264 -r 30 "$output_path_folder"
-        echo "Conversão de imagens para vídeo concluída!"
-        ;;
-    *)
-        echo "Tipo de arquivo não suportado. Saindo."
-        exit 1
-        ;;
-esac
+converter "$arquivo_entrada" "$arquivo_saida"
