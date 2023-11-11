a5a76d1392c799aaff389c627ee24997ec741e49
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-11-11 14:04:52 -0300

    Update convert2.py

--- convert2.py
+++ convert2.py
@@ -1,15 +1,11 @@
 import subprocess
 
-def configurar_permissao():
-    # Configuração automática das permissões para o ImageMagick
-    magick_configure_path = "/etc/ImageMagick-6/policy.xml"
-    subprocess.run(["sed", "-i", "s/<policymap>/$POLICY <policymap>/g", magick_configure_path])
+# Função para instalar as dependências
+def instalar_dependencias():
+    subprocess.run(['apt-get', 'install', 'imagemagick', 'libmagickwand-dev', '-y'])
+    subprocess.run(['pip', 'install', 'Wand'])
 
-def restaurar_permissao():
-    # Restauração das permissões originais do ImageMagick
-    magick_configure_path = "/etc/ImageMagick-6/policy.xml"
-    subprocess.run(["sed", "-i", "s/$POLICY <policymap>/<policymap>/g", magick_configure_path])
-
+# Função para converter arquivos
 def converter(arquivo_entrada, arquivo_saida):
     extensao_entrada = arquivo_entrada.split('.')[-1].lower()
     extensao_saida = arquivo_saida.split('.')[-1].lower()
@@ -18,20 +14,39 @@ def converter(arquivo_entrada, arquivo_saida):
         subprocess.run(['convert', '-density', '300', arquivo_entrada, '-quality', '100', arquivo_saida])
         print("Conversão de PDF para imagem concluída!")
 
+    elif extensao_entrada in ('docx', 'doc'):
+        subprocess.run(['libreoffice', '--headless', '--convert-to', 'pdf', '--outdir', f"{arquivo_saida.split('/')[0]}", arquivo_entrada])
+        print("Conversão de DOCX/DOC para PDF concluída!")
+
+    elif extensao_entrada == 'mp3':
+        subprocess.run(['ffmpeg', '-i', arquivo_entrada, arquivo_saida])
+        print("Conversão de MP3 para WAV concluída!")
+
+    elif extensao_entrada in ('mp4', 'avi'):
+        subprocess.run(['ffmpeg', '-i', arquivo_entrada, '-vf', 'fps=10,scale=320:-1:flags=lanczos', '-c:v', 'gif', arquivo_saida])
+        print("Conversão de MP4/AVI para GIF concluída!")
+
     elif extensao_entrada in ('jpg', 'jpeg', 'png', 'gif'):
         subprocess.run(['convert', arquivo_entrada, arquivo_saida])
-        print("Conversão de imagem para JPG concluída!")
+        print("Conversão de imagem para PDF concluída!")
 
-    # Adicione mais casos conforme necessário
+    elif extensao_entrada == 'txt':
+        subprocess.run(['cp', arquivo_entrada, arquivo_saida])
+        print("Cópia do arquivo de texto concluída!")
+
+    elif extensao_entrada == 'flac':
+        subprocess.run(['ffmpeg', '-i', arquivo_entrada, arquivo_saida])
+        print("Conversão de FLAC concluída!")
+
+    # Adicione mais casos para outros tipos de arquivo conforme necessário
 
     else:
-        print(f"Tipo de arquivo não suportado: {extensao_entrada}. Saindo.")
-        return
+        print("Tipo de arquivo não suportado. Saindo.")
 
-    restaurar_permissao()
+# Instala as dependências
+instalar_dependencias()
 
 # Exemplo de uso
-configurar_permissao()
 arquivo_entrada = input("Digite o nome do arquivo de entrada (com extensão): ")
 arquivo_saida = input("Digite o nome do arquivo de saída (com extensão): ")
 
