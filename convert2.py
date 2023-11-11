52fe5898a6d948d52fef4843f00dc1b864320549
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-11-11 14:13:47 -0300

    Update convert2.py

--- convert2.py
+++ convert2.py
@@ -1,9 +1,11 @@
 import subprocess
+import os
+from wand.image import Image
 
 # Função para instalar as dependências
 def instalar_dependencias():
-    subprocess.run(['apt-get', 'install', 'imagemagick', 'libmagickwand-dev', '-y'])
-    subprocess.run(['pip', 'install', 'Wand'])
+    subprocess.run(['sudo', 'apt-get', 'install', 'imagemagick', 'libmagickwand-dev', '-y'])
+    subprocess.run(['sudo', 'pip', 'install', 'Wand'])
 
 # Função para converter arquivos
 def converter(arquivo_entrada, arquivo_saida):
@@ -11,31 +13,32 @@ def converter(arquivo_entrada, arquivo_saida):
     extensao_saida = arquivo_saida.split('.')[-1].lower()
 
     if extensao_entrada == 'pdf':
-        subprocess.run(['convert', '-density', '300', arquivo_entrada, '-quality', '100', arquivo_saida])
+        with Image(filename=arquivo_entrada, resolution=300) as img:
+            img.save(filename=arquivo_saida)
         print("Conversão de PDF para imagem concluída!")
 
     elif extensao_entrada in ('docx', 'doc'):
-        subprocess.run(['libreoffice', '--headless', '--convert-to', 'pdf', '--outdir', f"{arquivo_saida.split('/')[0]}", arquivo_entrada])
+        subprocess.run(['libreoffice', '--headless', '--convert-to', 'pdf', '--outdir', f"{os.path.dirname(arquivo_saida)}", arquivo_entrada])
         print("Conversão de DOCX/DOC para PDF concluída!")
 
     elif extensao_entrada == 'mp3':
-        subprocess.run(['ffmpeg', '-i', arquivo_entrada, arquivo_saida])
+        subprocess.run(['sudo', 'ffmpeg', '-i', arquivo_entrada, arquivo_saida])
         print("Conversão de MP3 para WAV concluída!")
 
     elif extensao_entrada in ('mp4', 'avi'):
-        subprocess.run(['ffmpeg', '-i', arquivo_entrada, '-vf', 'fps=10,scale=320:-1:flags=lanczos', '-c:v', 'gif', arquivo_saida])
+        subprocess.run(['sudo', 'ffmpeg', '-i', arquivo_entrada, '-vf', 'fps=10,scale=320:-1:flags=lanczos', '-c:v', 'gif', arquivo_saida])
         print("Conversão de MP4/AVI para GIF concluída!")
 
     elif extensao_entrada in ('jpg', 'jpeg', 'png', 'gif'):
-        subprocess.run(['convert', arquivo_entrada, arquivo_saida])
+        subprocess.run(['sudo', 'convert', arquivo_entrada, arquivo_saida])
         print("Conversão de imagem para PDF concluída!")
 
     elif extensao_entrada == 'txt':
-        subprocess.run(['cp', arquivo_entrada, arquivo_saida])
+        subprocess.run(['sudo', 'cp', arquivo_entrada, arquivo_saida])
         print("Cópia do arquivo de texto concluída!")
 
     elif extensao_entrada == 'flac':
-        subprocess.run(['ffmpeg', '-i', arquivo_entrada, arquivo_saida])
+        subprocess.run(['sudo', 'ffmpeg', '-i', arquivo_entrada, arquivo_saida])
         print("Conversão de FLAC concluída!")
 
     # Adicione mais casos para outros tipos de arquivo conforme necessário
