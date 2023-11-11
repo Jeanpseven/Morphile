b1db5526c76e2d0240d4fd55c3a50eeeac2b8ab2
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-11-11 13:42:34 -0300

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,21 +1,36 @@
 import subprocess
-import os
-
-def instalar_imagemagick():
-    try:
-        subprocess.run(['convert', '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-    except FileNotFoundError:
-        subprocess.run(['sudo', 'apt-get', 'install', 'imagemagick'])
 
 def converter(arquivo_entrada, arquivo_saida):
     extensao_entrada = arquivo_entrada.split('.')[-1].lower()
     extensao_saida = arquivo_saida.split('.')[-1].lower()
 
-    instalar_imagemagick()  # Verifica e instala o ImageMagick se necessário
+    if extensao_entrada == 'pdf':
+        subprocess.run(['convert', '-density', '300', arquivo_entrada, '-quality', '100', arquivo_saida])
+        print("Conversão de PDF para imagem concluída!")
 
-    if extensao_entrada in ('jpg', 'jpeg', 'png', 'gif'):
+    elif extensao_entrada in ('docx', 'doc'):
+        subprocess.run(['libreoffice', '--headless', '--convert-to', 'pdf', '--outdir', f"{arquivo_saida.split('/')[0]}", arquivo_entrada])
+        print("Conversão de DOCX/DOC para PDF concluída!")
+
+    elif extensao_entrada == 'mp3':
+        subprocess.run(['ffmpeg', '-i', arquivo_entrada, arquivo_saida])
+        print("Conversão de MP3 para WAV concluída!")
+
+    elif extensao_entrada in ('mp4', 'avi'):
+        subprocess.run(['ffmpeg', '-i', arquivo_entrada, '-vf', 'fps=10,scale=320:-1:flags=lanczos', '-c:v', 'gif', arquivo_saida])
+        print("Conversão de MP4/AVI para GIF concluída!")
+
+    elif extensao_entrada in ('jpg', 'jpeg', 'png', 'gif'):
         subprocess.run(['convert', arquivo_entrada, arquivo_saida])
-        print(f"Conversão de {extensao_entrada.upper()} para {extensao_saida.upper()} concluída!")
+        print("Conversão de imagem concluída!")
+
+    elif extensao_entrada == 'txt':
+        subprocess.run(['cp', arquivo_entrada, arquivo_saida])
+        print("Cópia do arquivo de texto concluída!")
+
+    elif extensao_entrada == 'flac':
+        subprocess.run(['ffmpeg', '-i', arquivo_entrada, arquivo_saida])
+        print("Conversão de FLAC concluída!")
 
     # Adicione mais casos para outros tipos de arquivo conforme necessário
 
