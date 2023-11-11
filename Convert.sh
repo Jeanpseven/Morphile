4a98170dd02f1fddd74a50ef454c2f6d8d7e1643
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-11-11 12:22:47 -0300

    Create Convert.sh

---
+++ Convert.sh
@@ -0,0 +1,52 @@
+case "$extensao_entrada" in
+    pdf)
+        convert -density 300 "$arquivo_entrada" -quality 100 "$arquivo_saida"
+        echo "Conversão de PDF para imagem concluída!"
+        ;;
+    docx|doc)
+        libreoffice --headless --convert-to pdf --outdir $(dirname "$arquivo_saida") "$arquivo_entrada"
+        echo "Conversão de DOCX/DOC para PDF concluída!"
+        ;;
+    mp3)
+        ffmpeg -i "$arquivo_entrada" "$arquivo_saida"
+        echo "Conversão de MP3 para WAV concluída!"
+        ;;
+    mp4|avi)
+        ffmpeg -i "$arquivo_entrada" -vf "fps=10,scale=320:-1:flags=lanczos" -c:v gif "$arquivo_saida"
+        echo "Conversão de MP4/AVI para GIF concluída!"
+        ;;
+    jpg|jpeg|png|gif)
+        convert "$arquivo_entrada" "$arquivo_saida"
+        echo "Conversão de imagem para PDF concluída!"
+        ;;
+    txt)
+        cp "$arquivo_entrada" "$arquivo_saida"
+        echo "Cópia do arquivo de texto concluída!"
+        ;;
+    flac)
+        ffmpeg -i "$arquivo_entrada" -c:a mp3 "$arquivo_saida"
+        echo "Conversão de FLAC para MP3 concluída!"
+        ;;
+    folder)
+        read -p "Digite o caminho da pasta contendo imagens: " pasta_entrada
+        read -p "Digite a duração de cada imagem em segundos: " duracao
+        read -p "Digite o caminho completo do arquivo de saída (com extensão): " arquivo_saida
+
+        if [ ! -d "$pasta_entrada" ]; then
+            echo "Erro: Pasta de entrada não encontrada."
+            exit 1
+        fi
+
+        if [ -z "$arquivo_saida" ]; then
+            echo "Erro: Caminho de saída inválido."
+            exit 1
+        fi
+
+        ffmpeg -framerate 1/"$duracao" -pattern_type glob -i "$pasta_entrada/*.png" -c:v libx264 -r 30 "$arquivo_saida"
+        echo "Conversão de imagens para vídeo concluída!"
+        ;;
+    *)
+        echo "Tipo de arquivo não suportado. Saindo."
+        exit 1
+        ;;
+esac
